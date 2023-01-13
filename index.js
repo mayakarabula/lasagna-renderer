da070934d643c98628c21aaf914b9003c128ffe7
Maya Karabula-Stysiak <maya.karabula@contentful.com>
2023-01-13 21:49:50 +0100

    add character as a separate character

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const H = 200;
 const LAYERS = 199;
 const STACKING_DISTANCE = 4;
 
-let angle = 46;
+let characterX = 100;
+let characterY = 400;
+let sceneAngle = 46;
+let characterAngle = 181;
 const MIN_ANGLE = 20;
 const MAX_ANGLE = 70;
 const ANGLE_STEP = 10;
@@ -14,18 +17,27 @@ const ANGLE_STEP = 10;
 window.onload = () => {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-  const image = document.getElementById("source");
+  const scene = document.getElementById("source");
+  const character = document.getElementById("girl");
 
   ctx.imageSmoothingEnabled = false;
-  ctx.fillStyle = "#5d8f63";
+  ctx.fillStyle = "#799573";
 
   document.addEventListener("keydown", function (event) {
-    if (event.keyCode == 37 && angle > MIN_ANGLE) {
-      // left
-      angle -= ANGLE_STEP;
-    } else if (event.keyCode == 39 && angle < MAX_ANGLE) {
-      //right
-      angle += ANGLE_STEP;
+    if (event.key === "ArrowLeft" && sceneAngle > MIN_ANGLE) {
+      sceneAngle -= ANGLE_STEP;
+      characterAngle -= ANGLE_STEP;
+    } else if (event.key === "ArrowRight" && sceneAngle < MAX_ANGLE) {
+      sceneAngle += ANGLE_STEP;
+      characterAngle += ANGLE_STEP;
+    } else if (event.key === "w") {
+      characterY -= ANGLE_STEP;
+    } else if (event.key === "s") {
+      characterY += ANGLE_STEP;
+    } else if (event.key === "a") {
+      characterAngle -= ANGLE_STEP;
+    } else if (event.key === "d") {
+      characterAngle += ANGLE_STEP;
     }
   });
 
@@ -34,16 +46,26 @@ window.onload = () => {
   setInterval(() => {
     ctx.fillRect(0, 0, 500, 500);
 
-    const offsetX = -100;
+    const offsetX = -70;
     const offsetY = 100;
 
     for (let i = 0; i <= LAYERS; i++) {
       ctx.drawImage(
-        getFrame(image, i, angle),
+        getFrame(scene, i, sceneAngle, W, H, LAYERS),
         offsetX,
         offsetY - i * STACKING_DISTANCE,
         W * SCALE,
         H * SCALE
+      );
+    }
+
+    for (let i = 0; i <= 40; i++) {
+      ctx.drawImage(
+        getFrame(character, i, characterAngle, 40, 40, 39),
+        characterX,
+        characterY - i * STACKING_DISTANCE,
+        40 * SCALE,
+        40 * SCALE
       );
     }
   }, intervalTime);
@@ -51,28 +73,28 @@ window.onload = () => {
 
 // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 
-const getFrame = (source, frameIndex, angle) => {
+const getFrame = (source, frameIndex, angle, w, h, layers) => {
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
 
   const coefficient = Math.sqrt(2);
-  canvas.width = W * coefficient;
-  canvas.height = H * coefficient;
+  canvas.width = w * coefficient;
+  canvas.height = h * coefficient;
 
-  const centerX = W / 2 + (canvas.width - W) / 2;
-  const centerY = H / 2 + (canvas.height - H) / 2;
+  const centerX = w / 2 + (canvas.width - w) / 2;
+  const centerY = h / 2 + (canvas.height - h) / 2;
   ctx.translate(centerX, centerY);
 
   ctx.rotate((angle * Math.PI) / 180);
 
   const sourceX = -1; // this is offset to avoid artefacts from rotation, couldn't find a better way
-  const sourceY = -1 + H * (LAYERS - frameIndex);
-  const sourceWidth = W;
-  const sourceHeight = H;
-  const targetX = -W / 2; // start of drawing is moved to center so we need to offset by half
-  const targetY = -H / 2;
-  const targetWidth = W;
-  const targetHeight = H;
+  const sourceY = -1 + h * (layers - frameIndex);
+  const sourceWidth = w;
+  const sourceHeight = h;
+  const targetX = -w / 2; // start of drawing is moved to center so we need to offset by half
+  const targetY = -h / 2;
+  const targetWidth = w;
+  const targetHeight = h;
 
   ctx.drawImage(
     source,
